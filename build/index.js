f9256f2d59c1089e911c6f1e2297f4d828024ad3
Brixton <lkw06182000@hotmail.com>
2022-08-02 14:34:48 +0800

    Done implementing First array sorting algo

--- build/index.js
+++ build/index.js
@@ -1,2 +1,4 @@
 "use strict";
 console.log('hello world');
+console.log('hello world');
+console.log('hello world 123sadasdsasdasdasdsad');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('hello world');
+class Sorter {
+    // Data that we want to sort
+    // collection: number[];
+
+    // constructor(collection: number[]){
+    //     this.collection = collection;
+    // }
+
+    // More clearer code
+    constructor(public collection: number[]){};
+
+    // Function
+    sort(): void {
+        const { length } = this.collection;
+
+        for(let i = 0; i < length; i++) {
+            // - length is to prevent from looping over the whole aray
+            for(let j = 0; j < length - i - 1; j++) {
+                // Compare left with right element
+                if(this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+
+
